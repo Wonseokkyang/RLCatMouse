acd362821287c6f23d1acf6815929e2cba6f39f8
Wonseokkyang <Wonseokkyang@gmail.com>
2020-11-24 10:20:13 -0500

    added toggle for debug printing step by step

--- main.py
+++ main.py
@@ -50,53 +50,64 @@ def main():
     cheesePos = env.cheese.pos
     board = env.mazeList
     env.renderWindow = True
+
+    ## DEBUGING Step by step
+    debug = True
 #lets impliment regular, 1step with both cat and mouse first then apply n-step.
 #1-step with cat
     while True:
-        print('\nCLICK to start loop.')
-        env.win.getMouse()
+        if debug:
+            print('\nCLICK to start loop.')
+            env.win.getMouse()
         print('==At start of loop, cat and mouse information:==')
         myCat.printInfo()
         myMouse.printInfo()
 
-        print('\nCLICK to let mouse choose action.')
-        env.win.getMouse()
+        if debug:
+            print('\nCLICK to let mouse choose action.')
+            env.win.getMouse()
         # print('Calling mouse.chooseRandom with catpos mousepos cheese pos:', myCat.pos, myMouse.pos, cheesePos)
         mouseAction = myMouse.chooseAction(board, myCat.pos, myMouse.pos, cheesePos)
         mouseImmediateReward = env.moveMouse(mouseAction)
         print('immediate reward:', mouseImmediateReward)
         print('myMouse.q_table:', myMouse.q_table)
 
-        print('\nCLICK to let cat choose action.')
-        env.win.getMouse()
+        if debug:
+            print('\nCLICK to let cat choose action.')
+            env.win.getMouse()
         # print('Calling cat.chooseRandom with catpos mousepos cheese pos:', myCat.pos, myMouse.pos, cheesePos)
         catAction = myCat.chooseAction(board, myCat.pos, myMouse.pos, cheesePos)
         catImmediateReward = env.moveCat(catAction)
         print('catAction:', catAction)
         print('immediate reward:', catImmediateReward)
 
-        print('\nCLICK to get feedback from environment.')
-        env.win.getMouse()
+        if debug:
+            print('\nCLICK to get feedback from environment.')
+            env.win.getMouse()
         #get feedback from the environment
         catPos, catReward, mousePos, mouseReward, done = env.turnEnd()
         print('catPos:', catPos, 'catImmediateReward:', catImmediateReward, 'mousePos:', mousePos, 'mouseImmediateReward:', mouseImmediateReward, 'done:', done)
 
-        print('\nCLICK to update agent Brain with positions.')
-        env.win.getMouse()
+        if debug:
+            print('\nCLICK to update agent Brain with positions.')
+            env.win.getMouse()
         # Update agent's brains to reflect board positions after move
         myMouse.updateBrain(catPos, catReward, mousePos, mouseReward)
         myCat.updateBrain(catPos, catReward, mousePos, mouseReward)
 
         myCat.printInfo()
         myMouse.printInfo()
-        print('\nCLICK to start learnLast step for both agents.')
-        env.win.getMouse()
+
+        if debug:
+            print('\nCLICK to start learnLast step for both agents.')
+            env.win.getMouse()
         #immediate learning of step taken
         myMouse.learnLast(mouseImmediateReward)
         myCat.learnLast(catImmediateReward)
         myCat.printInfo()
         myMouse.printInfo()
-        print('\nCLICK to continue.')
+        if debug:
+            print('\nCLICK to continue.')
 
         #if something got caught, execute learning of agents
         if done: 
